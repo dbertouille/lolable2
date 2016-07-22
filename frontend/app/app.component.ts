c1bb7762473e2e662b1b54ddd5bf161d78538506
dbertouille <dbertouille@gmail.com>
2016-07-21 22:52:05 -0500

    Adding skeleton angular app.

---
+++ frontend/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'lolable',
+  template: `
+    <h1>{{name}}</h1>
+  `
+})
+
+export class AppComponent {
+  name = 'Lolable';
+}
