2d5816cf7abc43ed3f94642957884c069de4b985
dbertouille <dbertouille@gmail.com>
2016-07-25 17:14:18 -0500

    Mock the menu.

--- frontend/app/app.component.ts
+++ frontend/app/app.component.ts
@@ -7,8 +7,19 @@ import globals = require('./globals');
 
 @Component({
   selector: 'lolable',
-  template: `
-    <img src="{{logourl}}"/>
+  template:`
+    <div id="content" align="center">
+        <div id="header">
+            <img src="{{logourl}}"/>
+	    <div id="topmenu">
+            <md-button>About</md-button>
+            <md-button>Archive</md-button>
+            <md-button>Podcasts</md-button>
+            </div>
+        </div>
+        <div id="footer">
+        </div>
+    </div>
   `,
   providers: [ConfigurationService]
 })
